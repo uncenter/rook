db2c07841fa72321b53bb8b53c009e46244cbd7e
uncenter <47499684+uncenter@users.noreply.github.com>
2024-03-07 13:16:40 -0500

    refactor: reuse rook & bishop logic for queen

--- src/lib.ts
+++ src/lib.ts
@@ -39,182 +39,173 @@ export class Square {
 			return x < 8 && x > -1 && y < 8 && y > -1;
 		};
 
-		switch (this.piece) {
-			case "pawn": {
-				if (this.team === "black") {
-					const forwards = [[x, y + 1]];
-					if (y === 1) forwards.push([x, y + 2]);
+		if (this.piece === "pawn") {
+			if (this.team === "black") {
+				const forwards = [[x, y + 1]];
+				if (y === 1) forwards.push([x, y + 2]);
 
-					for (const fwd of forwards) {
-						if (isRealCoord(fwd) && board[toPos(fwd)].team === "none") {
-							moves.push(fwd);
-						}
-					}
-
-					const diags = [
-						[x - 1, y + 1],
-						[x + 1, y + 1],
-					];
-					for (const diag of diags) {
-						if (
-							isRealCoord(diag) &&
-							!["none", this.team].includes(board[toPos(diag)].team)
-						) {
-							moves.push(diag);
-						}
-					}
-				} else if (this.team === "white") {
-					const forwards = [[x, y - 1]];
-					if (y === 6) forwards.push([x, y - 2]);
-
-					for (const fwd of forwards) {
-						if (isRealCoord(fwd) && board[toPos(fwd)].team === "none") {
-							moves.push(fwd);
-						}
-					}
-
-					const diags = [
-						[x - 1, y - 1],
-						[x + 1, y - 1],
-					];
-					for (const diag of diags) {
-						if (
-							isRealCoord(diag) &&
-							!["none", this.team].includes(board[toPos(diag)].team)
-						) {
-							moves.push(diag);
-						}
+				for (const fwd of forwards) {
+					if (isRealCoord(fwd) && board[toPos(fwd)].team === "none") {
+						moves.push(fwd);
 					}
 				}
-				break;
-			}
-			case "knight": {
-				moves = [
-					[x - 2, y + 1],
-					[x - 2, y - 1],
-					[x + 2, y + 1],
-					[x + 2, y - 1],
-					[x + 1, y + 2],
-					[x + 1, y - 2],
-					[x - 1, y + 2],
-					[x - 1, y - 2],
-				].filter(isRealCoord);
 
-				break;
-			}
-			case "bishop": {
-				const checks = {
-					left: true,
-					right: true,
-				};
-				for (let i = y + 1; i < 8; i++) {
-					if (checks.right) {
-						const right = [x + (i - y), i];
-						if (!isRealCoord(right) || board[toPos(right)].team === this.team) {
-							checks.right = false;
-						} else {
-							if (["none", this.team].includes(board[toPos(right)].team)) {
-								checks.right = false;
-							}
-							moves.push(right);
-						}
-					}
-					if (checks.left) {
-						const left = [x - (i - y), i];
-						if (!isRealCoord(left) || board[toPos(left)].team === this.team) {
-							checks.left = false;
-						} else {
-							if (!["none", this.team].includes(board[toPos(left)].team)) {
-								checks.left = false;
-							}
-							moves.push(left);
-						}
-					}
-				}
-				checks.left = true;
-				checks.right = true;
-				for (let i = y - 1; i > -1; i--) {
-					if (checks.right) {
-						const right = [x + (i - y), i];
-						if (!isRealCoord(right) || board[toPos(right)].team === this.team) {
-							checks.right = false;
-						} else {
-							if (this.isOtherTeam(board[toPos(right)].team)) {
-								checks.right = false;
-							}
-							moves.push(right);
-						}
-					}
-					if (checks.left) {
-						const left = [x - (i - y), i];
-						if (!isRealCoord(left) || board[toPos(left)].team === this.team) {
-							checks.left = false;
-						} else {
-							if (this.isOtherTeam(board[toPos(left)].team)) {
-								checks.left = false;
-							}
-							moves.push(left);
-						}
-					}
-				}
-				break;
-			}
-			case "rook": {
-				for (let i = y + 1; i < 8; i++) {
-					if (board[toPos([x, i])].team === this.team) {
-						break;
-					}
-					moves.push([x, i]);
-					if (this.isOtherTeam(board[toPos([x, i])].team)) {
-						break;
-					}
-				}
-				for (let i = y - 1; i > -1; i--) {
-					if (board[toPos([x, i])].team === this.team) {
-						break;
-					}
-					moves.push([x, i]);
-					if (this.isOtherTeam(board[toPos([x, i])].team)) {
-						break;
-					}
-				}
-				for (let j = x + 1; j < 8; j++) {
-					if (board[toPos([j, y])].team === this.team) {
-						break;
-					}
-					moves.push([j, y]);
-					if (this.isOtherTeam(board[toPos([j, y])].team)) {
-						break;
-					}
-				}
-				for (let j = x - 1; j > -1; j--) {
-					if (board[toPos([j, y])].team === this.team) {
-						break;
-					}
-					moves.push([j, y]);
-					if (this.isOtherTeam(board[toPos([j, y])].team)) {
-						break;
-					}
-				}
-				break;
-			}
-			case "king": {
-				moves = [
-					[x, y + 1],
-					[x, y - 1],
-
-					[x + 1, y],
-					[x - 1, y],
-
-					[x + 1, y + 1],
-					[x - 1, y - 1],
-
-					[x + 1, y - 1],
+				const diags = [
 					[x - 1, y + 1],
-				].filter(isRealCoord);
+					[x + 1, y + 1],
+				];
+				for (const diag of diags) {
+					if (
+						isRealCoord(diag) &&
+						!["none", this.team].includes(board[toPos(diag)].team)
+					) {
+						moves.push(diag);
+					}
+				}
+			} else if (this.team === "white") {
+				const forwards = [[x, y - 1]];
+				if (y === 6) forwards.push([x, y - 2]);
 
-				break;
+				for (const fwd of forwards) {
+					if (isRealCoord(fwd) && board[toPos(fwd)].team === "none") {
+						moves.push(fwd);
+					}
+				}
+
+				const diags = [
+					[x - 1, y - 1],
+					[x + 1, y - 1],
+				];
+				for (const diag of diags) {
+					if (
+						isRealCoord(diag) &&
+						!["none", this.team].includes(board[toPos(diag)].team)
+					) {
+						moves.push(diag);
+					}
+				}
 			}
+		}
+		if (this.piece === "knight") {
+			moves = [
+				[x - 2, y + 1],
+				[x - 2, y - 1],
+				[x + 2, y + 1],
+				[x + 2, y - 1],
+				[x + 1, y + 2],
+				[x + 1, y - 2],
+				[x - 1, y + 2],
+				[x - 1, y - 2],
+			].filter(isRealCoord);
+		}
+		if (this.piece === "bishop" || this.piece === "queen") {
+			const checks = {
+				left: true,
+				right: true,
+			};
+			for (let i = y + 1; i < 8; i++) {
+				if (checks.right) {
+					const right = [x + (i - y), i];
+					if (!isRealCoord(right) || board[toPos(right)].team === this.team) {
+						checks.right = false;
+					} else {
+						if (["none", this.team].includes(board[toPos(right)].team)) {
+							checks.right = false;
+						}
+						moves.push(right);
+					}
+				}
+				if (checks.left) {
+					const left = [x - (i - y), i];
+					if (!isRealCoord(left) || board[toPos(left)].team === this.team) {
+						checks.left = false;
+					} else {
+						if (!["none", this.team].includes(board[toPos(left)].team)) {
+							checks.left = false;
+						}
+						moves.push(left);
+					}
+				}
+			}
+			checks.left = true;
+			checks.right = true;
+			for (let i = y - 1; i > -1; i--) {
+				if (checks.right) {
+					const right = [x + (i - y), i];
+					if (!isRealCoord(right) || board[toPos(right)].team === this.team) {
+						checks.right = false;
+					} else {
+						if (this.isOtherTeam(board[toPos(right)].team)) {
+							checks.right = false;
+						}
+						moves.push(right);
+					}
+				}
+				if (checks.left) {
+					const left = [x - (i - y), i];
+					if (!isRealCoord(left) || board[toPos(left)].team === this.team) {
+						checks.left = false;
+					} else {
+						if (this.isOtherTeam(board[toPos(left)].team)) {
+							checks.left = false;
+						}
+						moves.push(left);
+					}
+				}
+			}
+		}
+		if (this.piece === "rook" || this.piece === "queen") {
+			for (let i = y + 1; i < 8; i++) {
+				if (board[toPos([x, i])].team === this.team) {
+					break;
+				}
+				moves.push([x, i]);
+				if (this.isOtherTeam(board[toPos([x, i])].team)) {
+					break;
+				}
+			}
+			for (let i = y - 1; i > -1; i--) {
+				if (board[toPos([x, i])].team === this.team) {
+					break;
+				}
+				moves.push([x, i]);
+				if (this.isOtherTeam(board[toPos([x, i])].team)) {
+					break;
+				}
+			}
+			for (let j = x + 1; j < 8; j++) {
+				if (board[toPos([j, y])].team === this.team) {
+					break;
+				}
+				moves.push([j, y]);
+				if (this.isOtherTeam(board[toPos([j, y])].team)) {
+					break;
+				}
+			}
+			for (let j = x - 1; j > -1; j--) {
+				if (board[toPos([j, y])].team === this.team) {
+					break;
+				}
+				moves.push([j, y]);
+				if (this.isOtherTeam(board[toPos([j, y])].team)) {
+					break;
+				}
+			}
+		}
+		if (this.piece === "king") {
+			moves = [
+				[x, y + 1],
+				[x, y - 1],
+
+				[x + 1, y],
+				[x - 1, y],
+
+				[x + 1, y + 1],
+				[x - 1, y - 1],
+
+				[x + 1, y - 1],
+				[x - 1, y + 1],
+			].filter(isRealCoord);
 		}
 		return moves.map((move) => toPos(move)).filter(isRealMove);
 	}
